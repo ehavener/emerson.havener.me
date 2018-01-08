d51fafbfe9ded016002d3282dc0010ffd7df5d1c
ehavener <emersonhavener@gmail.com>
2018-01-07 19:58:51 -0800

    home nearly complete for mobile

--- src/Home.js
+++ src/Home.js
@@ -7,15 +7,18 @@ class Home extends Component {
       <div className="home" id="Home">
         <div className="content">
           <div className="name">
-            <h1>Emerson</h1>
-            <h1>Havener</h1>
+            <h1>
+              Emerson
+              <br></br>
+              Havener
+            </h1>
           </div>
           <h2>Web Developer</h2>
           <div className="social-icons">
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
-            <i className="fa fa-github" aria-hidden="true"></i>
-            <i className="fa fa-dribbble" aria-hidden="true"></i>
-            <i className="fa fa-codepen" aria-hidden="true"></i>
+            <a href="https://www.linkedin.com/in/emerson-havener-8737a1121/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="https://github.com/ehavener" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+            <a href="https://dribbble.com/emersonhavener" target="_blank"><i className="fa fa-dribbble" aria-hidden="true"></i></a>
+            <a href="https://codepen.io/ehavener/" target="_blank"><i className="fa fa-codepen" aria-hidden="true"></i></a>
           </div>
         </div>
       </div>
