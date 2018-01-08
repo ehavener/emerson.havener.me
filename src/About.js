06d96644078a94a744298c2bb4da887a1dadcd07
ehavener <emersonhavener@gmail.com>
2018-01-07 21:21:57 -0800

    refactored all content sections to be mobile first

--- src/About.js
+++ src/About.js
@@ -6,7 +6,12 @@ class About extends Component {
     return (
       <div className="about" id="About">
         <h1>About Me</h1>
-        <p>I'm a student at UNR and a prospective web developer.</p>
+        <p> I first took interest to graphic design back in 2013. Since then, I've ventured into website design & development. I'm currently pursuing a B.S. in Computer Science & Engineering with a minor
+                in Mathematics at the University of Nevada, Reno.
+        </p>
+        <p>I expect to graduate in May of 2019. I'm actively
+        seeking employment as a junior developer, and sharpening my skills with freelance work in the meantime.
+        Contact or hire me!</p>
       </div>
     );
   }
