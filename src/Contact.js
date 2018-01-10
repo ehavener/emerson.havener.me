ad7ab232534ae2ba3a6d1b6689a9deefad25f92e
ehavener <emersonhavener@gmail.com>
2018-01-09 18:43:16 -0800

    added mountian vector to footer

--- src/Contact.js
+++ src/Contact.js
@@ -14,23 +14,23 @@ class Contact extends Component {
               <textarea placeholder="Message" name="message"></textarea>
               <button value="submit" type="button">Send</button>
             </form>
-          </div>
+          </div> {/*}
           <div className="card">
             <div className="social-icons">
-            <a href="https://www.linkedin.com/in/emerson-havener-8737a1121/" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-linkedin" title="LinkedIn" aria-hidden="true"></i>
-            </a>
-            <a href="https://github.com/ehavener" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-github" title="Github" aria-hidden="true"></i>
-            </a>
-            <a href="https://dribbble.com/emersonhavener" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-dribbble" title="Dribble" aria-hidden="true"></i>
-            </a>
-            <a href="https://codepen.io/ehavener/" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-codepen" title="Codepen" aria-hidden="true"></i>
-            </a>
-          </div>
-          </div>
+              <a href="https://www.linkedin.com/in/emerson-havener-8737a1121/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-linkedin" title="LinkedIn" aria-hidden="true"></i>
+              </a>
+              <a href="https://github.com/ehavener" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-github" title="Github" aria-hidden="true"></i>
+              </a>
+              <a href="https://dribbble.com/emersonhavener" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-dribbble" title="Dribble" aria-hidden="true"></i>
+              </a>
+              <a href="https://codepen.io/ehavener/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-codepen" title="Codepen" aria-hidden="true"></i>
+              </a>
+             </div>
+          </div>*/}
         </div>
       </div>
     );
--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import p1 from './images/parallax/1.png';
+import p2 from './images/parallax/2.png';
+import p3 from './images/parallax/3.png';
+import p4 from './images/parallax/4.png';
+import p5 from './images/parallax/5.png';
+import p6 from './images/parallax/6.png';
 import './Footer.css';
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <p>© 2017 Emerson Havener • All Rights Reserved</p>
+        <div class="multi-bg-example"><p>© 2018 Emerson Havener</p></div>
       </div>
+
     );
   }
 }
-
+/**/
 export default Footer;
