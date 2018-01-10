f71e3aa8aff047026e80b21561c1c42efaeaa2af
ehavener <emersonhavener@gmail.com>
2018-01-09 20:03:49 -0800

    polished contact form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './normalize.css';
-import App from './App';
 import 'font-awesome/css/font-awesome.min.css';
+import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
