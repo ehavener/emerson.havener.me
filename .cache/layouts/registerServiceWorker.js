d74c2acbe189d6ee7c0d6fa938fe375c3e5602c1
Emerson Havener <emersonhavener@gmail.com>
2018-03-06 02:48:49 -0800

    removed cached build dirs

--- .cache/layouts/registerServiceWorker.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "D:/www/emerson.havener.me/src/layouts/registerServiceWorker.js"
-  import data from "D:\\www\\emerson.havener.me\\.cache\\json\\layout-register-service-worker.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
